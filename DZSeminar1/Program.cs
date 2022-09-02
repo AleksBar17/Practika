b9d44d6b9c35389135af5ca43719243b03286725
Aleksandra <alez-brize@mail.ru>
2022-09-02 14:29:48 +0300

    первое задание большее и меньшее число

---
+++ DZSeminar1/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите первое число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 > num2) 
+{ 
+    Console.WriteLine("большее число " + num1);
+    Console.WriteLine("меньшее число " + num2);
+}
+
+else
+{
+    Console.WriteLine("большее число " + num2);
+    Console.WriteLine("меньшее число " + num1);
+}
+
