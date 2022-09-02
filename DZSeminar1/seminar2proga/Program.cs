564fdf88672300e271a6913bbed14a0da01bc4b2
Aleksandra <alez-brize@mail.ru>
2022-09-02 15:58:25 +0300

    задания

---
+++ DZSeminar1/seminar2proga/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Введите первое число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите третье число");
+int num3 = Convert.ToInt32(Console.ReadLine());
+{
+if (num1 > num2) 
+
+if (num1 > num3)
+{
+if (num2 > num3)
+
+    Console.WriteLine("max" + num1);
+}
+
+else 
+
+{
+    Console.WriteLine("max" + num3);
+    
+}
+
+else 
+{ 
+    Console.WriteLine("max" + num2);
+}
+}
---
+++ DZSeminar1/seminar2proga/zadanie3/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("ВВедите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+{
+if (num % 2 == 0)
+{
+    Console.WriteLine("это четное число");
+}
+
+else 
+{
+    Console.WriteLine("Это не четное число");
+}
+}
